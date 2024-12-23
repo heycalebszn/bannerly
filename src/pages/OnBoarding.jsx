bfe544eede2f579e44a7c39d401481dd7414341c
LENI4C <leni4c.dev@gmail.com>
2024-12-23 19:02:04 +0100

    spacing fix

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -6,24 +6,24 @@ import { FaGithub } from "react-icons/fa";
 
 const OnBoarding = () => {
     return (
-        <div className="flex flex-col items-center justify-center text-center relative text-white gap-y-8 max-lg:gap-y-12 leading-tight">
-            <div className="flex flex-col text-center z-[999px] relative items-center justify-center gap-y-6 max-lg:gap-y-10">
+        <div className="flex flex-col items-center justify-center text-center relative text-white">
+            <div className="flex flex-col z-[999px] relative">
                 <Header />
 
-                <h1 className=" text-white font-bold lg:text-6xl md:text-4xl text-xl">
+                <h1 className=" text-lg text-white font-bold  md:text-4xl md:leading-loose">
                     Get your{" "}
                     <span className="bg-clip-text text-transparent bg-gradient-to-b from-purple-300 to-purple-600">
                         customized
                     </span>{" "}
                     Social banner.
                 </h1>
-                <p className=" text-lg text-transparent text-white md:text-3xl lg:text-4xl ">
+                <p className=" text-sm text-transparent text-white md:text-3xl  mt-[2vh] ">
                     for Software Developers & Designers.
                 </p>
             </div>
 
             <Link to={"/get-started"}>
-                <button className="bg-white text-purple-700 md:text-lg lg:text-xl p-2 lg:px-4 lg:py-3 rounded-lg font-semibold hover:text-white hover:bg-purple-700 transition ease-in align-middle">
+                <button className="bg-white text-purple-700 md:text-lg lg:text-xl p-2 lg:px-4 lg:py-3 rounded-lg font-semibold hover:text-white hover:bg-purple-700 transition ease-in align-middle mt-[2vh] md:mt-[5vh]">
                     get started! <span className="pl-3">👩‍🍳</span>
                 </button>
             </Link>
